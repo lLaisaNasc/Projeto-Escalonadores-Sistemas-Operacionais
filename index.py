0210383d564183b30046170718442af55d05a763
Laisa Nascimento <llaisanasc@gmail.com>
2023-09-28 10:43:03 -0300

    stj e rr atualizados

--- index.py
+++ index.py
@@ -4,13 +4,12 @@ import matplotlib.pyplot as plt
 # -- Ler o arquivo --
 doc = open('teste.txt', 'r', encoding='utf8')
 nome_escalonamento = doc.readline().strip()
+
+# -- Dados do Processo --
 quantum = 0
 prioridade_maior = 0
 prioridade_menor = 0
 processo_dados = []
-processsos_finalizados = []
-processos_prontos = []
-
 
 # -- Idenfica o escalonamento --
 def identifica_escalonamento():
@@ -74,154 +73,29 @@ def calcula_media_tempo_espera(tempos_espera, total_processos):
 
 # -- Algoritmo FCFS --
 def utiliza_first_come_first_served():
-  processo_dados = [linha.replace('\u00a0', '').strip() for linha in doc.readlines() if linha.strip() != ""]
-  processos_string = []
-  processos_inteiro = []
 
-  for n in processo_dados:
-      processos_string.append(n.split())
-
-  for processo in processos_string:
-      aux = []
-      for dado in processo:
-         dado = eval(dado)
-         aux.append(dado)
-
-      processos_inteiro.append(aux)
-
-  print(processos_inteiro)
- 
-  tabela_dados = [
-      ["Pid", "AT", "BT", "CT", "TAT", "WT"]
-    ]
-
-  processos_ordenados = sorted(processos_inteiro, key=lambda x: x[1])
-
-# -- Variáveis do processo --
-  id_processo = 0
-  tempo_chegada = 0
-  tempo_cpu = 0
-  tempo_conclusão = 0
-  tempo_sistema = 0
-  tempo_espera = 0
-  tempos_sistema = []
-  tempos_espera = []
-      
-  for processo in processos_ordenados:
-
-      processo_calculado = []
-
-      id_processo = processo[0]
-      processo_calculado.append(id_processo)
-
-      tempo_chegada = processo[1]
-      processo_calculado.append(tempo_chegada)
-
-      tempo_cpu = processo[2]
-      processo_calculado.append(tempo_cpu)
-
-      tempo_conclusão += tempo_cpu
-      processo_calculado.append(tempo_conclusão)
-
-      tempo_sistema = tempo_conclusão - tempo_chegada
-      processo_calculado.append(tempo_sistema)
-
-      tempo_espera = tempo_sistema - tempo_cpu
-      processo_calculado.append(tempo_espera)
-
-      tabela_dados.append(processo_calculado)
-      tempos_sistema.append(tempo_sistema)
-      tempos_espera.append(tempo_espera)
-
-  media_tempo_sistema = calcula_media_tempo_sistema(tempos_sistema, len(processos_ordenados))
-  media_tempo_espera = calcula_media_tempo_espera(tempos_espera, len(processos_ordenados))
-
-  tabela_formatada = tabulate(tabela_dados, headers="firstrow", tablefmt="grid")
-  with open("saida.txt", "w") as arquivo:
-    arquivo.write(tabela_formatada)
-    arquivo.write("\n")
-    arquivo.write(media_tempo_sistema)
-    arquivo.write("\n")
-    arquivo.write(media_tempo_espera)
-
-    # criar_linha_do_tempo([4, 7, 8, 10, 15], ['1', '2', '3', '4', '5'], ['4', '3', '1', '2', '3'])
-
-def utilizar_sjf():
     processo_dados = [linha.replace('\u00a0', '').strip() for linha in doc.readlines() if linha.strip() != ""]
-    processos_string = []
-    processos_inteiro = []
+    processos_inteiro = [[eval(dado) for dado in linha.split()] for linha in processo_dados]
 
-    for n in processo_dados:
-        processos_string.append(n.split())
+    tabela_dados = [["Pid", "AT", "BT", "CT", "TAT", "WT"]]
 
-    for processo in processos_string:
-        aux = []
-        for dado in processo:
-            dado = eval(dado)
-            aux.append(dado)
-
-        processos_inteiro.append(aux)
-
-    print(processos_inteiro)
-    
-    tabela_dados = [
-        ["Pid", "AT", "BT", "CT", "TAT", "WT"]
-        ]
-
-    processos_ordenados = sorted(processos_inteiro, key=lambda x: x[2])
-
-    # -- Variáveis do processo --
-    id_processo = 0
-    tempo_chegada = 0
-    tempo_cpu = 0
-    tempo_conclusão = processos_ordenados[0][1]
-    tempo_sistema = 0
-    tempo_espera = 0
+    tempo_conclusao = 0
     tempos_sistema = []
     tempos_espera = []
 
-    while (len(processos_ordenados) > len(processsos_finalizados)):
-        tempo_atual = 0
+    for id_processo, tempo_chegada, tempo_cpu in processos_inteiro:
 
-        for processo in processos_ordenados:
-            if processo[1] == tempo_atual:
-                processos_prontos.append(processo)
-        
-        processos_prontos = sorted(processos_prontos, key=lambda x: x[2])
+        tempo_conclusao += tempo_cpu
+        tempo_sistema = tempo_conclusao - tempo_chegada
+        tempo_espera = tempo_sistema - tempo_cpu
 
-        for processo in processos_prontos:
+        tempos_sistema.append(tempo_sistema)
+        tempos_espera.append(tempo_espera)
 
-            processo_calculado = []
+        tabela_dados.append([id_processo, tempo_chegada, tempo_cpu, tempo_conclusao, tempo_sistema, tempo_espera])
 
-            id_processo = processo[0]
-            processo_calculado.append(id_processo)
-
-            tempo_chegada = processo[1]
-            processo_calculado.append(tempo_chegada)
-
-            tempo_cpu = processo[2]
-            processo_calculado.append(tempo_cpu)
-
-            tempo_conclusão += tempo_cpu
-            processo_calculado.append(tempo_conclusão)
-
-            tempo_sistema = tempo_conclusão - tempo_chegada
-            processo_calculado.append(tempo_sistema)
-
-            tempo_espera = tempo_sistema - tempo_cpu
-            processo_calculado.append(tempo_espera)
-
-            tabela_dados.append(processo_calculado)
-            tempos_sistema.append(tempo_sistema)
-            tempos_espera.append(tempo_espera)
-
-            processsos_finalizados.append(processo)
-            processos_prontos.remove(0)
-
-        tempo_atual += 1
-
-    media_tempo_sistema = calcula_media_tempo_sistema(tempos_sistema, len(processos_ordenados))
-    media_tempo_espera = calcula_media_tempo_espera(tempos_espera, len(processos_ordenados))
+    media_tempo_sistema = calcula_media_tempo_sistema(tempos_sistema, len(processos_inteiro))
+    media_tempo_espera = calcula_media_tempo_espera(tempos_espera, len(processos_inteiro))
 
     tabela_formatada = tabulate(tabela_dados, headers="firstrow", tablefmt="grid")
     with open("saida.txt", "w") as arquivo:
@@ -232,12 +106,126 @@ def utilizar_sjf():
         arquivo.write(media_tempo_espera)
 
 
-#while(processos_ordenados.length() > processsos_finalizados):
-      
-     
+    # criar_linha_do_tempo([4, 7, 8, 10, 15], ['1', '2', '3', '4', '5'], ['4', '3', '1', '2', '3'])
 
+# -- Algoritmo JSF ---
+def utilizar_sjf():
+    processo_dados = [linha.replace('\u00a0', '').strip() for linha in doc.readlines() if linha.strip() != ""]
+    processos_string = []
+    processos_inteiro = []
 
+    for n in processo_dados:
+        processos_string.append(n.split())
 
+    for processo in processos_string:
+        aux = [eval(dado) for dado in processo]
+        processos_inteiro.append(aux)
+
+    tabela_dados = [
+        ["Pid", "AT", "BT", "CT", "TAT", "WT"]
+    ]
+
+    # Inicializando variáveis
+    tempo_atual = 0
+    tempos_sistema = []
+    tempos_espera = []
+    processos_finalizados = []
+
+    while processos_inteiro:
+
+        processos_prontos = [p for p in processos_inteiro if p[1] <= tempo_atual]
+        
+        if not processos_prontos:
+            tempo_atual += 1
+            continue
+
+        processo = min(processos_prontos, key=lambda x: x[2])
+
+        id_processo, tempo_chegada, tempo_cpu = processo
+        tempo_conclusao = tempo_atual + tempo_cpu
+        tempo_sistema = tempo_conclusao - tempo_chegada
+        tempo_espera = tempo_sistema - tempo_cpu
+
+        tempos_sistema.append(tempo_sistema)
+        tempos_espera.append(tempo_espera)
+
+        tabela_dados.append([id_processo, tempo_chegada, tempo_cpu, tempo_conclusao, tempo_sistema, tempo_espera])
+
+        tempo_atual = tempo_conclusao
+        processos_inteiro.remove(processo)
+        processos_finalizados.append(processo)
+
+    tabela_dados = sorted(tabela_dados[1:], key=lambda x: x[1]) 
+    tabela_dados.insert(0, ["Pid", "AT", "BT", "CT", "TAT", "WT"]) 
+
+    media_tempo_sistema = calcula_media_tempo_sistema(tempos_sistema, len(processos_finalizados))
+    media_tempo_espera = calcula_media_tempo_espera(tempos_espera, len(processos_finalizados))
+
+    tabela_formatada = tabulate(tabela_dados, headers="firstrow", tablefmt="grid")
+    with open("saida.txt", "w") as arquivo:
+        arquivo.write(tabela_formatada)
+        arquivo.write("\n")
+        arquivo.write(media_tempo_sistema)
+        arquivo.write("\n")
+        arquivo.write(media_tempo_espera)
+
+def utiliza_round_robin():
+    quantum = eval(doc.readline().strip())
+    processo_dados = [linha.replace('\u00a0', '').strip() for linha in doc.readlines() if linha.strip() != ""]
+    processos = [[eval(dado) for dado in linha.split()] for linha in processo_dados]
+    tabela_dados = [["Pid", "AT", "BT", "CT", "TAT", "WT"]]
+
+    tempo_atual = 0
+    fila = []
+
+    while processos or fila:
+        processos_chegados = [p for p in processos if p[1] <= tempo_atual]
+        for p in processos_chegados:
+            p.append(p[2]) 
+            fila.append(p)
+            processos.remove(p)
+
+        if fila: 
+            processo = fila.pop(0)
+            id_processo, tempo_chegada, tempo_bt, tempo_cpu_original = processo
+            
+            tempo_executado = min(tempo_bt, quantum)
+            tempo_atual += tempo_executado
+            tempo_bt -= tempo_executado
+
+            processos_chegados = [p for p in processos if p[1] <= tempo_atual]
+            for p in processos_chegados:
+                p.append(p[2]) 
+                fila.append(p)
+                processos.remove(p)
+            
+            if tempo_bt > 0:
+                fila.append([id_processo, tempo_chegada, tempo_bt, tempo_cpu_original])
+            else:
+                tempo_conclusao = tempo_atual
+                tempo_sistema = tempo_conclusao - tempo_chegada
+                tempo_espera = tempo_sistema - tempo_cpu_original
+                tabela_dados.append([id_processo, tempo_chegada, tempo_cpu_original, tempo_conclusao, tempo_sistema, tempo_espera])
+        else:
+            tempo_atual += 1 
+
+    media_tempo_sistema = calcula_media_tempo_sistema([row[4] for row in tabela_dados[1:]], len(tabela_dados) - 1)
+    media_tempo_espera = calcula_media_tempo_espera([row[5] for row in tabela_dados[1:]], len(tabela_dados) - 1)
+
+    tabela_formatada = tabulate(tabela_dados, headers="firstrow", tablefmt="grid")
+    with open("saida.txt", "w") as arquivo:
+        arquivo.write(tabela_formatada)
+        arquivo.write("\n")
+        arquivo.write(media_tempo_sistema)
+        arquivo.write("\n")
+        arquivo.write(media_tempo_espera)
+
+# -- Algoritmo Priority ---
+def utiliza_prioridade():
+    ...
 
 identifica_escalonamento()
-utiliza_first_come_first_served()
+#utiliza_first_come_first_served()
+#utilizar_sjf()
+utiliza_round_robin()
+
